f7b0801e5746f43b46f4e9ef5d134dd6e2704904
shangrex <rexhuang881019@gmail.com>
2021-08-16 20:37:23 +0800

    Add Server initialization

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-var React = require('react');
+const express = require('express')
 
+const app = express()
+
+
+app.get('/', function(req, res) {
+    res.sendFile('src/game.html', {root: __dirname })
+})
+
+
+app.listen(3000, function () {
+	console.log('listen 3000!');
+})
